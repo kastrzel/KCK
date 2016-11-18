78f195bf98a8750b623dfd189c0341b7dce9e691
Yagira <yagiraa@gmail.com>
2016-11-18 16:51:30 +0100

    Odpowiedzi glosowe.

--- README.md
+++ README.md
@@ -8,11 +8,16 @@
 
 **PyQt5: https://www.riverbankcomputing.com/software/pyqt/download5**
 
+Uruchamianie:
 
-Jak ktoś chce sprawdzić u siebie:
+*1) Gotowy plik* https://github.com/kastrzel/KCK/releases/tag/v0.1.0
+   
+_**lub**_
+
+*2) Repozytorium:*
    1. Pobieramy https://github.com/huntersan9/Py3kAiml
    2. Wypakowujemy, przechodzimy do wypakowanego folderu za pomoca *cmd* lub *powershell* i używamy komendy ```python setup.py install```
-   3. Instalujemy *PyQt5* za pomoca ```pip3 install PyQt5```
-   4. Klonujemy repozytorium i uruchamiamy test.py (Można też pobrać: https://github.com/kastrzel/KCK/releases/tag/v0.1.0)
+   3. Instalujemy wymagane moduły: ```pip3 install pyqt5``` ```pip install pydub``` ```pip install pyaudio``` ```pip install gtts```
+   4. Klonujemy repozytorium i uruchamiamy test.py.
 
 
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import aiml
 import sys
-
+import gtts
+from pydub import AudioSegment
+from pydub.playback import play
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 #from tkinter import *
 #from PIL import ImageTk, Image
+
 
 class Window(QtWidgets.QWidget):
 
@@ -15,57 +18,74 @@ class Window(QtWidgets.QWidget):
 
     def init_ui(self):
 
-        #self.setFixedSize(250,220)
-        self.b = QtWidgets.QPushButton('Mów')
-        self.l = QtWidgets.QLabel('Tu pojawi sie odpowiedź.')
-        self.l2 = QtWidgets.QLabel()
-        self.checkbox = QtWidgets.QCheckBox("Kobieta")
+        # self.setFixedSize(250,220)
+        self.speak = QtWidgets.QPushButton('Mów')
+        self.answer = QtWidgets.QLabel('Tu pojawi sie odpowiedź.')
+        self.imagelabel = QtWidgets.QLabel()
+        self.isFemale = QtWidgets.QCheckBox("Kobieta")
+        self.isOrderComplete = QtWidgets.QCheckBox("Zamówienie złożone")
 
         self.textInput = QtWidgets.QLineEdit()
         self.textInput.setPlaceholderText("Co chcesz powiedzieć?")
         self.textInput.setToolTip("Wpisz to co chcesz powiedzieć papieżowi.")
         h_box = QtWidgets.QHBoxLayout()
-        h_box.addStretch()
-        h_box.addWidget(self.l) 
-        h_box.addStretch()
+        h_box.addWidget(self.isFemale) #kobieta
+        h_box.addWidget(self.isOrderComplete) #rachunek
 
         v_box = QtWidgets.QVBoxLayout()
-        v_box.addWidget(self.l2) #image
-        v_box.addWidget(self.l) #text
-        v_box.addWidget(self.textInput) #textinput
-        v_box.addWidget(self.b) #button
-        v_box.addWidget(self.checkbox) #taki tam checkbox
+        v_box.addWidget(self.imagelabel)  # image
+        v_box.addWidget(self.answer)  # text
+        v_box.addWidget(self.textInput)  # textinput
+        v_box.addWidget(self.speak)  # button
+        v_box.addLayout(h_box) # takie tam checkboxy
 
         self.setLayout(v_box)
         self.setWindowTitle('Kelner v 0.0.1')
 
-        self.l.setMargin(0)
-        self.l.setIndent(0)
+        self.answer.setMargin(0)
+        self.answer.setIndent(0)
 
-
-        self.b.clicked.connect(self.btn_click)
+        self.speak.clicked.connect(self.btn_click)
 
         app_X = 250
         app_Y = 250
         screenGeo = QtWidgets.QDesktopWidget().screenGeometry()
 
-        #self.setGeometry((screenGeo.width() / 2) - (app_X / 2),
-                    #(screenGeo.height() / 2) - (app_Y / 2))
-        self.l.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
-        self.l2.setAlignment(QtCore.Qt.AlignCenter)
-        self.l2.setPixmap(QtGui.QPixmap('kelner.png'))
-        #print(screen_X)
+        # self.setGeometry((screenGeo.width() / 2) - (app_X / 2),
+        #(screenGeo.height() / 2) - (app_Y / 2))
+        self.answer.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
+        self.imagelabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.imagelabel.setPixmap(QtGui.QPixmap('kelner.png'))
+        # print(screen_X)
 
         self.show()
 
-
     def btn_click(self):
         sentence = self.textInput.text()
-        if self.checkbox.isChecked():
+
+        if self.isFemale.isChecked():
             k.setPredicate("plec", "k")
         else:
             k.setPredicate("plec", "m")
-        self.l.setText("Kelner: " + k.respond(sentence))
+
+        if self.isFemale.isChecked():
+            k.setPredicate("rachunek", "yes")
+        else:
+            k.setPredicate("rachunek", "no")
+        
+        response = k.respond(sentence)
+        self.answer.setText("Kelner: " + response)
+        app.processEvents()
+        if response:
+            self.playsound(response)
+
+    def playsound(self, response):
+        tts = gtts.gTTS(text=response, lang='pl')
+        tts.save("kelner.mp3")
+        song = AudioSegment.from_mp3("kelner.mp3")
+        play(song)
+
+
 
 k = aiml.Kernel()
 k.learn("std-startup.xml")
@@ -77,11 +97,11 @@ app = QtWidgets.QApplication(sys.argv)
 a_window = Window()
 
 app_icon = QtGui.QIcon()
-app_icon.addFile('kelner.png', QtCore.QSize(16,16))
-app_icon.addFile('kelner.png', QtCore.QSize(24,24))
-app_icon.addFile('kelner.png', QtCore.QSize(32,32))
-app_icon.addFile('kelner.png', QtCore.QSize(48,48))
-app_icon.addFile('kelner.png', QtCore.QSize(256,256))
+app_icon.addFile('kelner.png', QtCore.QSize(16, 16))
+app_icon.addFile('kelner.png', QtCore.QSize(24, 24))
+app_icon.addFile('kelner.png', QtCore.QSize(32, 32))
+app_icon.addFile('kelner.png', QtCore.QSize(48, 48))
+app_icon.addFile('kelner.png', QtCore.QSize(256, 256))
 app.setWindowIcon(app_icon)
 
 sys.exit(app.exec_())
